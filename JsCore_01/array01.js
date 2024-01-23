d4184b7d28b07e2f1e764b960ba2b151c0b7e418
kanonkartik <kart00ik@gmail.com>
2024-01-23 09:42:21 +0530

    01

--- JsCore_01/array01.js
+++ JsCore_01/array01.js
@@ -35,3 +35,19 @@ console.log(myarrayA.join("NaN"))
 console.log(arrA.push(myarrayA))
 
 console.log("HERE IT IS",arrA)
+
+console.log(myarray.concat(arrB))
+
+const flatArray =  [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+console.log(flatArray.flat(Infinity))
+
+console.log(Array.isArray("Hitesh"))//asking??
+console.log(Array.from("Hitesh"))//[ 'H', 'i', 't', 'e', 's', 'h' ]
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
